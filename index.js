9152023c0dae1dc05bf36f4cd85e4c09f5f8159b
Shakiba Alam <shakibaalam092@gmail.com>
2022-05-24 11:58:26 +0600

    get users

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ async function run() {
             res.send(result)
         })
 
+        //get all users info
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users)
+        });
+
         //login or creating user info
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
